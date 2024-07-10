d303dd48e886fcae104557a72faf2f702bb2331f
加藤尚暉 <kato@kato-MacBookAir.local>
2024-07-10 22:31:07 +0900

    fix

---
+++ lib/pages/todo_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ToDoListPage extends StatefulWidget {
+  const ToDoListPage({super.key});
+
+  @override
+  State<ToDoListPage> createState() => _ToDoListPageState();
+}
+
+class _ToDoListPageState extends State<ToDoListPage> {
+  List<String> toDoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      body: Center(
+        child: Text('TODOリストはここに表示されます'),
+      ),
+    );
+  }
+}

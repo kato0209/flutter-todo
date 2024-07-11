06e58ebbff81a19554e9afd6a73ad01d21928c60
加藤尚暉 <kato@kato-MacBookAir.local>
2024-07-11 22:37:32 +0900

    fix

--- lib/pages/todo_list.dart
+++ lib/pages/todo_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../features/todo/todo_item.dart';
+import '../features/todo/todo_add.dart';
 
 class ToDoListPage extends StatefulWidget {
   const ToDoListPage({super.key});
@@ -8,16 +10,41 @@ class ToDoListPage extends StatefulWidget {
 }
 
 class _ToDoListPageState extends State<ToDoListPage> {
-  List<String> toDoList = [];
+  List<String> toDoList = ["test"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.green,
         title: Text('リスト一覧'),
       ),
-      body: Center(
-        child: Text('TODOリストはここに表示されます'),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoItem(
+              item: toDoList[index],
+              onDelete: () {
+                setState(() {
+                  toDoList.removeAt(index);
+                });
+              });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return ToDoAddPage();
+            }),
+          );
+          if (newListText != null) {
+            setState(() {
+              toDoList.add(newListText);
+            });
+          }
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
